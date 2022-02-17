0fca06539f1c1c54a879a9fd584e4c35f2923fb8
katrinaandreev1 <77636957+katrinaandreeva3006@users.noreply.github.com>
2022-02-17 13:03:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+teksts = input("Ievadiet vardu : ")
+i = 1
+while i <= 22:
+  print(teksts)
+
+  i += 1
+a = 1
+
+while a <= 22:
+    print(str(a) + str(teksts))
+    a += 1
+print((teksts + " ") * 22)
